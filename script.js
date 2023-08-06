fe8686201fc13d93871d9fb233d4b3d66403a276
ranju <ranjue16@gmail.com>
2023-08-06 05:44:06 +0530

    Feature01: Save the list

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 const historyList = [];
-currentList = [];
+let currentList = [];
 
-function addToList() {
-    // Get the input value
-    const inputElement = document.getElementById("listItemInput");
-    const itemText = inputElement.value;
+// Load saved tasks from local storage
+if (localStorage.getItem('savedList')) {
+    currentList = JSON.parse(localStorage.getItem('savedList'));
+    updateList();
+}
 
-    if (itemText.trim() !== "") {
-        // Create a new list
+function updateList() {
+    const itemList = document.getElementById("itemList");
+        // Check if the itemList element exists
+    if (!itemList) {
+        console.error("Element with id 'itemList' not found.");
+        return;
+        }
+      
+    itemList.innerHTML = "";
+    // Clear historyList before updating
+    historyList.length = 0;
+
+
+    currentList.forEach((itemText) => {
         const listItem = document.createElement("li");
         listItem.className = "list-group-item d-flex justify-content-between align-items-center";
 
-        // Create a checkbox
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         checkbox.className = "form-check-input me-1";
         listItem.appendChild(checkbox);
 
-        // Create a label for the checkbox
         const label = document.createElement("label");
         label.className = "form-check-label";
         label.innerText = itemText;
@@ -25,47 +36,46 @@ function addToList() {
         label.style.padding = "0.3em 0.3em 0.3em";
         listItem.appendChild(label);
 
-        // Append the new item to the list
-        const itemList = document.getElementById("itemList");
-        itemList.appendChild(listItem);
-        currentList.push(itemText);
+        const deleteButton = document.createElement("button");
+        deleteButton.className = "btn btn-close";
+        deleteButton.style = "--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .25rem; --bs-btn-font-size: .50rem;"
+        listItem.appendChild(deleteButton);
 
-        // Clear the input field after adding the item
-        inputElement.value = "";
-        
-
-
-        // Add an event listener to the checkbox
         checkbox.addEventListener("change", function () {
             if (checkbox.checked) {
                 label.style.textDecoration = "line-through";
             } else {
                 label.style.textDecoration = "none";
             }
-        
         });
-        
-    // Create a delete button for each list item
-    const deleteButton = document.createElement("button");
-    deleteButton.className = "btn btn-close";
-    deleteButton.style = "--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .25rem; --bs-btn-font-size: .50rem;"
-    listItem.appendChild(deleteButton);
 
-    // Add event listener to the delete button
-    deleteButton.addEventListener("click", function () {
-        // Remove the list item from the DOM when the delete button is clicked
-        listItem.remove();
-        currentList.pop(listItem);
+        deleteButton.addEventListener("click", function () {
+            const index = currentList.indexOf(itemText);
+            if (index > -1) {
+                currentList.splice(index, 1);
+                updateList();
+            }
+        });
+
+        itemList.appendChild(listItem);
+        // Update historyList with the saved tasks
+        historyList.push(itemText);
     });
-
-    
-    }
-    //Append the History List
-    historyList.push(itemText);
 }
 
+function addToList() {
+    const inputElement = document.getElementById("listItemInput");
+    const itemText = inputElement.value;
 
-//Add items buy pressing enter
+    if (itemText.trim() !== "") {
+        currentList.push(itemText);
+        updateList();
+
+        inputElement.value = "";
+    }
+    // Append the History List
+    historyList.push(itemText);
+}
 
 function handleKeyPress(event) {
     if (event.key === "Enter") {
@@ -73,42 +83,33 @@ function handleKeyPress(event) {
     }
 }
 
-// clear all items button
 function clearAll() {
     const itemList = document.getElementById("itemList");
-    itemList.innerHTML = ""; // Clear the list by removing all its content
-};
+    itemList.innerHTML = "";
+    currentList = [];
+}
 
-// Flag to track the visibility of history.
 let isHistoryShown = false;
 
-
 function toggleHistory() {
-    // Show the history by displaying the contents of the itemListHistory array
     const historyContainer = document.getElementById('history-container');
+    // Check if the historyContainer element exists
+    if (!historyContainer) {
+        console.error("Element with id 'history-container' not found.");
+        return;
+      }
 
-    if (isHistoryShown){
+    if (isHistoryShown) {
         historyContainer.style.display = 'none';
     } else {
-        historyContainer.innerHTML = historyList.join('<br>'); // Display the history contents
-        historyContainer.style.display = 'block'; // Show the history
-
+        historyContainer.innerHTML = historyList.join('<br>');
+        historyContainer.style.display = 'block';
     }
-    //toggle the flag
+
     isHistoryShown = !isHistoryShown;
-    
 }
 
-function downloadList() {
-    // Create a Blob with the list data
-    const listData = currentList.join('\n');
-    const blob = new Blob([listData], { type: 'text/plain' });
-
-    // Create a temporary link to trigger the download
-    const downloadLink = document.createElement('a');
-    downloadLink.href = URL.createObjectURL(blob);
-    downloadLink.download = 'list_history.txt';
-    downloadLink.click();
+function saveList() {
+    // Save the currentList to local storage
+    localStorage.setItem('savedList', JSON.stringify(currentList));
 }
-
-localStorage.setItem('currentList', JSON.stringify(currentList));
